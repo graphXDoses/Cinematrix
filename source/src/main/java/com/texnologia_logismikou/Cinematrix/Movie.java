747fd3fa5139e88951f0b98f0f520a04312db0b3
Bonamerck <antoni234lp@gmail.com>
2024-04-22 23:43:01 +0300

    add Movie.java, Room.java

---
+++ source/src/main/java/com/texnologia_logismikou/Cinematrix/Movie.java
@@ -0,0 +1,11 @@
+package com.texnologia_logismikou.Cinematrix;
+
+public class Movie {
+	private String name;
+	private int Rel_Date;
+	private int Duration;
+	private String genre;
+	private String Description;
+	private String director;
+	private String trailer_link;
+}
