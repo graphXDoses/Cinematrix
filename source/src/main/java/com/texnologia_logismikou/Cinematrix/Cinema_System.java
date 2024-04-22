333069cbc35cb6c62735401abb2ab15cc184cd28
LoneSurvivor4 <michaelefthimiadis@gmail.com>
2024-04-23 00:20:23 +0300

    add Cinema_System.java , Ticket.java

---
+++ source/src/main/java/com/texnologia_logismikou/Cinematrix/Cinema_System.java
@@ -0,0 +1,13 @@
+package com.texnologia_logismikou.Cinematrix;
+
+import java.util.ArrayList;
+
+public class Cinema_System {
+	
+	private ArrayList<Movie[]> movie = new ArrayList<>();
+	private ArrayList<Room[]> room = new ArrayList<>();
+	private ArrayList<Screening[]> screening = new ArrayList<>();
+	private ArrayList<Customer[]> customer = new ArrayList<>();
+	private ArrayList<Ticket[]> ticket = new ArrayList<>();
+	
+}
