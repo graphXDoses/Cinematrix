61d60903d1f8bcd63a166cf7f5aafc8fa59ec5f2
LoneSurvivor4 <michaelefthimiadis@gmail.com>
2024-05-22 17:29:14 +0300

    add ViewSelectedPayment.vvp

--- source/src/main/java/module-info.java
+++ source/src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module com.texnologia_logismikou.Cinematrix {
-    requires javafx.controls;
-    exports com.texnologia_logismikou.Cinematrix;
+	exports com.texnologia_logismikou.Cinematrix;
+
+	requires com.texnologia_logismikou.Cinematrix;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires org.junit.jupiter.api;
 }
