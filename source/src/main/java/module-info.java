217d8ee27544ebb313e0d9726a51a5d02664771b
graphXDoses <xridoses@gmail.com>
2024-06-05 14:53:09 +0300

    This seems OK

--- source/src/main/java/module-info.java
+++ source/src/main/java/module-info.java
@@ -1,4 +1,7 @@
-module com.texnologia_logismikou.Cinematrix {
+module Cinematrix {
     requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.texnologia_logismikou.Cinematrix to javafx.fxml;
     exports com.texnologia_logismikou.Cinematrix;
 }
