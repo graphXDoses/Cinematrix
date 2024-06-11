e68d078d3d225555469d702b412219102614b90f
graphXDoses <xridoses@gmail.com>
2024-06-11 14:16:06 +0300

    Configs for runnable jar [WIP].

--- source/src/main/java/module-info.java
+++ source/src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module Cinematrix {
+//	requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-	requires javafx.graphics;
 
     opens com.texnologia_logismikou.Cinematrix to javafx.fxml;
     exports com.texnologia_logismikou.Cinematrix;
-//	requires javafx.graphics;
 }
